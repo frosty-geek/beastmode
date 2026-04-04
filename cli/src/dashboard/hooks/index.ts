7c2520b4cf5bf0369cd7f82969d60f4fb93e3097
Michael Schmidt <test@test.com>
2026-04-04 10:32:19 +0200

    feat(app-rewrite): clean up barrel exports and key hints

--- cli/src/dashboard/hooks/index.ts
+++ cli/src/dashboard/hooks/index.ts
@@ -1,14 +1,5 @@
-export { useKeyboardNav } from "./use-keyboard-nav.js";
-export type { KeyboardNavState } from "./use-keyboard-nav.js";
-
-export { useCancelFlow } from "./use-cancel-flow.js";
-export type { CancelFlowState, CancelFlowResult } from "./use-cancel-flow.js";
-
 export { useGracefulShutdown } from "./use-graceful-shutdown.js";
 export type { GracefulShutdownState } from "./use-graceful-shutdown.js";
-
-export { useToggleAll } from "./use-toggle-all.js";
-export type { ToggleAllState } from "./use-toggle-all.js";
 
 export { useTerminalSize } from "./use-terminal-size.js";
 export type { TerminalSize } from "./use-terminal-size.js";
